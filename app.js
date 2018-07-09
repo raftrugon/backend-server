16e980141f6d3db15c290146468de1c826713081
Rafael Trujillo <raftrugon@alum.us.es>
2018-07-09 19:41:21 +0200

    Primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// Required
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicializar variables
+var app = express();
+
+//Conexion a la BBDD
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Base de datos online: \x1b[32m%s\x1b[0m', 'online');
+
+})
+
+// Rutas
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    })
+});
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
